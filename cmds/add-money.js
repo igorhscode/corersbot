0dac9b589693d8b2f797db06373545a441b1b22c
igorhscode <59455439+igorhscode@users.noreply.github.com>
2020-02-11 19:04:46 +0300

    Update add-money.js

--- cmds/add-money.js
+++ cmds/add-money.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
 module.exports.run = async (bot,message,args) => {
-    let em1 = message.guild.emojis.find(emoji => emoji.name === "yes")
-    let em2 = message.guild.emojis.find(emoji => emoji.name === "animated_cross")
+    let em2 = message.guild.emojis.find(emoji => emoji.name === "yes")
+    let em1 = message.guild.emojis.find(emoji => emoji.name === "animated_cross")
     if (!message.member.hasPermission('MANAGE_GUILD')) { // if message.author / member does not have the permission MANAGE_GUILD, return.
-        return message.channels.send(`${em2} Вы не можете использовать эту команду!`).then(msg => {
+        return message.channels.send(`${em1} Вы не можете использовать эту команду!`).then(msg => {
             setTimeout(() => {
                 msg.delete()
             }, 2500); // delete after 2.5 seconds.
@@ -15,12 +15,12 @@ module.exports.run = async (bot,message,args) => {
     let argsUser 
     if (member) argsUser = member.user
     else argsUser = message.author
-    if (isNaN(args[0])) return message.channel.send(`${em2} ${message.author}, вам нужно ввести допустимое число для прибавления.`) // if args[0] (first input) is not a number, return.
+    if (isNaN(args[0])) return message.channel.send(`${em1} ${message.author}, вам нужно ввести допустимое число для прибавления.`) // if args[0] (first input) is not a number, return.
     db.add(`coins_${argsUser.id}`, args[0])
     let bal = await db.fetch(`coins_${argsUser.id}`)
 
     let embed = new Discord.RichEmbed()
-    .setAuthor(`${em1} Добавлены 🌞 монеты!`)
+    .setAuthor(`${em2} Добавлены 🌞 монеты!`)
     .addField("Игрок", argsUser.username)
     .addField(`Количество`, `${args[0]} ☀️ монет`)
     .addField(`Баланс:`, `${bal} ☀️ монет`)
