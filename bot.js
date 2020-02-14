34b36110ce4ad1ba0c7b7e799d793f40c11b2478
igorhscode <59455439+igorhscode@users.noreply.github.com>
2020-02-14 22:15:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -180,7 +180,7 @@ message.channel.send(temp)
       }
       let curxp = xp[message.author.id].xp;
       let curlvl = xp[message.author.id].level;
-      let nxtLvl = xp[message.author.id].level * 400;
+      let nxtLvl = xp[message.author.id].level * 300;
       xp[message.author.id].xp =  curxp + xpAdd;
       if(nxtLvl <= xp[message.author.id].xp){
         xp[message.author.id].level = curlvl + 1;
